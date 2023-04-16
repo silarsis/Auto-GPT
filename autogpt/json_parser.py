7eb98849cab4d7eb3d06e602a5d0f9deca39dff3
Kevin Littlejohn <kevin@littlejohn.id.au>
2023-04-16 00:29:21 +0000

    make the json fixer more robust

--- autogpt/json_parser.py
+++ autogpt/json_parser.py
@@ -5,6 +5,7 @@ from autogpt.call_ai_function import call_ai_function
 from autogpt.config import Config
 from autogpt.json_utils import correct_json
 from autogpt.logger import logger
+from autogpt.llm_utils import create_chat_completion
 
 cfg = Config()
 
@@ -78,35 +79,49 @@ def fix_and_parse_json(
         
 def fix_json(json_str: str, schema: str) -> str:
     """Fix the given JSON string to make it parseable and fully compliant with the provided schema."""
-    # Try to fix the JSON using GPT:
-    function_string = "def fix_json(json_str: str, schema:str=None) -> str:"
-    args = [f"'''{json_str}'''", f"'''{schema}'''"]
-    description_string = (
-        "Fixes the provided JSON string to make it parseable"
-        " and fully compliant with the provided schema.\n If an object or"
-        " field specified in the schema isn't contained within the correct"
-        " JSON, it is omitted.\n This function is brilliant at guessing"
-        " when the format is incorrect."
-    )
-
-    # If it doesn't already start with a "`", add one:
-    if not json_str.startswith("`"):
-        json_str = "```json\n" + json_str + "\n```"
-    result_string = call_ai_function(
-        function_string, args, description_string, model=cfg.fast_llm_model
-    )
-    logger.debug("------------ JSON FIX ATTEMPT ---------------")
-    logger.debug(f"Original JSON: {json_str}")
-    logger.debug("-----------")
-    logger.debug(f"Fixed JSON: {result_string}")
-    logger.debug("----------- END OF FIX ATTEMPT ----------------")
-
+    model = cfg.smart_llm_model
+    messages = [
+        {
+            "role": "system",
+            "content": f"Please adjust the following json to be valid: \n {json_str}",
+        },
+    ]
+    fixed_json_str = create_chat_completion(model=cfg.fast_llm_model, messages=messages, temperature=0)
     try:
-        json.loads(result_string)  # just check the validity
-        return result_string
-    except:  # noqa: E722
-        # Get the call stack:
-        # import traceback
-        # call_stack = traceback.format_exc()
-        # print(f"Failed to fix JSON: '{json_str}' "+call_stack)
+        json.loads(fixed_json_str)
+        return fixed_json_str
+    except: # noqa: E722
         return "failed"
+
+    # # Try to fix the JSON using GPT:
+    # function_string = "def fix_json(json_str: str, schema:str=None) -> str:"
+    # args = [f"'''{json_str}'''", f"'''{schema}'''"]
+    # description_string = (
+    #     "Fixes the provided JSON string to make it parseable"
+    #     " and fully compliant with the provided schema.\n If an object or"
+    #     " field specified in the schema isn't contained within the correct"
+    #     " JSON, it is omitted.\n This function is brilliant at guessing"
+    #     " when the format is incorrect."
+    # )
+
+    # # If it doesn't already start with a "`", add one:
+    # if not json_str.startswith("`"):
+    #     json_str = "```json\n" + json_str + "\n```"
+    # result_string = call_ai_function(
+    #     function_string, args, description_string, model=cfg.fast_llm_model
+    # )
+    # logger.debug("------------ JSON FIX ATTEMPT ---------------")
+    # logger.debug(f"Original JSON: {json_str}")
+    # logger.debug("-----------")
+    # logger.debug(f"Fixed JSON: {result_string}")
+    # logger.debug("----------- END OF FIX ATTEMPT ----------------")
+
+    # try:
+    #     json.loads(result_string)  # just check the validity
+    #     return result_string
+    # except:  # noqa: E722
+    #     # Get the call stack:
+    #     # import traceback
+    #     # call_stack = traceback.format_exc()
+    #     # print(f"Failed to fix JSON: '{json_str}' "+call_stack)
+    #     return "failed"
